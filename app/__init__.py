a2f560e79717371943f66a29dde6cc45acbf20ba
Daniel Hine <dchine@outlook.com>
2022-03-08 22:14:31 -0500

    Added cookie_name

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SESSION_COOKIE_NAME'] = "my_session"
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
